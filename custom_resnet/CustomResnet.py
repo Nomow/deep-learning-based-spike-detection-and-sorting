071daa8e7ac47103ff07720277afbfc75a7aa614
Nomow <alvils.sture@gmail.com>
2019-05-30 16:20:04 +0300

    removed figures

--- custom_resnet/CustomResnet.py
+++ custom_resnet/CustomResnet.py
@@ -109,7 +109,6 @@ class ResNet(nn.Module):
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        #self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
         self.avgpool = nn.AdaptiveAvgPool1d(1)
         self.fc = torch.nn.Sequential(torch.nn.Dropout(0.5),
                                       torch.nn.Linear(256 * block.expansion, num_classes))
@@ -156,7 +155,6 @@ class ResNet(nn.Module):
         x = self.layer1(x)
         x = self.layer2(x)
         x = self.layer3(x)
-        #x = self.layer4(x)
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
@@ -905,7 +903,6 @@ class ft_net(nn.Module):
         x = self.model.layer1(x)
         x = self.model.layer2(x)
         x = self.model.layer3(x)
-        #x = self.layer4(x)
         x = self.model.avgpool(x)
         x = x.view(x.size(0), x.size(1))
         x = self.model.fc(x)
